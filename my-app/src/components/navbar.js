d159acebc9b40a2811094219b221296732166e6b
alanc <alanchen8647@gmail.com>
2024-10-19 13:53:27 -0400

    nav started

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,16 +1,18 @@
 
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import Image from 'react-bootstrap/Image';
+import avatar from '../images/avatar.jpeg';
+import '../style/navbar.css';
 
 function StackedExample() {
   return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
+    <Nav defaultActiveKey="/home" className="flex-column" style={{ width: '15%',textAlign:'center'}}>
+      <Image src={avatar} style={{width:'100%'}}roundedCircle />
+      <Nav.Link href="/home">Quest Board</Nav.Link>
+      <Nav.Link eventKey="link-1">Community</Nav.Link>
+      <Nav.Link eventKey="link-2">My Advanture</Nav.Link>
+      <Nav.Link eventKey="link-3">Schedule</Nav.Link>
     </Nav>
   );
 }
