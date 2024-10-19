ebbd5ff2d05150ed628fcc2c722361ee978e966a
alanc <alanchen8647@gmail.com>
2024-10-19 14:37:53 -0400

    fixed configure

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import NavBar from './components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecentView from './components/recentView';
+import App from "./App"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <NavBar/>
-     <RecentView/>
+     <App/>
   </React.StrictMode>
 );
 
