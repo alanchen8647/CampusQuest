f25d1927be852b59136d00fff1864ebbfec260d0
bradleyhung <bradleyhung5570@gmail.com>
2024-10-20 02:04:15 -0400

    Updated questlist

--- my-app/src/components/questlist.js
+++ my-app/src/components/questlist.js
@@ -1,29 +1,30 @@
-import React from 'react' 
+import React from 'react';
 import '../style/questlist.css';
 import questImage from '../images/image.jpg';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/esm/Col';
-import { alignPropType } from 'react-bootstrap/esm/types';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function QuestList() {
-    return (
-      <Card className="box" style={{width: '100%', height: '175px'}}>
-        <Card.Body className='body'>
-        <div className="imageSection"> 
-        <Card.Img variant="top" src={questImage} style={{width:'70%', height: '85%'}}/> 
-        </div> 
-        <div className="textSection"> 
-        <Card.Title className='title'>Looking for Study Buddies</Card.Title> 
-          <Card.Text>          
-          </Card.Text> 
-        </div>
-         
-        </Card.Body>
-       
-        
-                    
-          
-      </Card>
+  return (
+      <Card className='container' style={{width: '98%'}}>
+          <Row className='row' >
+              <Col md={4}>
+                  <Card.Img  className="card-img" src={questImage} style={{width: '60%'}} alt="Quest" />
+              </Col>
+              <Col md={8}>
+                  <Card.Body>
+                      <Card.Title>Looking for companions for a museum trip to Metropolis on 10/12 2:00PM!</Card.Title>
+                      <Card.Text>
+                          Some quick example text to build on the card title and make up the
+                          bulk of the card's content.
+                      </Card.Text>
+                      <Card.Text>
+                          <small className="text-body-secondary">Last updated 3 mins ago</small>
+                      </Card.Text>
+                  </Card.Body>
+              </Col>
+          </Row>
+      </Card>        
     );
-  }
-  
+}
